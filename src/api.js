c393aa6b3f790b785a911166fcd34f5f4d973711
Tenacious <tenaciousmthembu.com>
2026-02-16 05:23:21 +0200

    404 error fixed?

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const BASE_URL = import.meta.env.VITE_API_URL
 
-const API = axios.create({ baseURL: BASE_URL, withCredentials: true, })
+const API = axios.create({ baseURL: BASE_URL + '/api', withCredentials: true, })
 
 
 API.interceptors.request.use((req) => {
