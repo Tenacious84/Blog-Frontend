427170cefbac38fe3c5385cad55366cd161ff58f
Tenacious <tenaciousmthembu.com>
2026-02-19 04:24:08 +0200

    Footer component ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import NavBar from './Components/NavBar/NavComponent'
 import { Outlet } from 'react-router-dom'
+import Footer from './Components/Footer/FooterComponent'
 
 function App() {
 
   return (
     <>
       <NavBar />
-
+      <Footer />
       <Outlet />
 
     </>
